b13dc40f90499cafb594f0db0d4392f436226d0d
Anastasia951 <n.ivanova504@gmail.com>
2023-03-20 01:43:38 +0300

    feat: add controls

--- src/index.js
+++ src/index.js
@@ -2,6 +2,37 @@ import './styles.scss'
 import { Game } from './Game'
 import { View } from './View'
 
+const CONTROLS = {
+  ArrowUp: 'ArrowUp',
+  ArrowLeft: 'ArrowLeft',
+  ArrowDown: 'ArrowDown',
+  ArrowRight: 'ArrowRight',
+}
+
+const controlsButtons = document.querySelector('[data-controls]')
+
+
+controlsButtons.addEventListener('click', event => {
+  let { direction } = event.target.dataset
+  if (!direction) return
+
+  switch (direction) {
+    case 'left': {
+      return immediateRender({ key: CONTROLS.ArrowLeft })
+    }
+    case 'up': {
+      return immediateRender({ key: CONTROLS.ArrowUp })
+    }
+    case 'right': {
+      return immediateRender({ key: CONTROLS.ArrowRight })
+    }
+    case 'down': {
+      return immediateRender({ key: CONTROLS.ArrowDown })
+    }
+  }
+})
+
+
 const game = new Game()
 game.init()
 const view = new View()
@@ -10,25 +41,27 @@ let activePiece = null
 view.render(game.field)
 
 
-document.addEventListener('keydown', event => {
+function immediateRender(event) {
   if (game.isGameOver && event.key === ' ') {
     game.isGameOver = false
     game.startGame(play)
   }
   if (activePiece) {
-    if (event.key === 'ArrowUp') {
+    if (event.key === CONTROLS.ArrowUp) {
       game.rotate()
-    } else if (event.key === 'ArrowDown') {
+    } else if (event.key === CONTROLS.ArrowDown) {
       game.movePieceDown()
-    } else if (event.key === 'ArrowLeft') {
+    } else if (event.key === CONTROLS.ArrowLeft) {
       game.movePieceLeft()
-    } else if (event.key === 'ArrowRight') {
+    } else if (event.key === CONTROLS.ArrowRight) {
       game.movePieceRight()
     }
     view.render(game.field)
     view.renderActivePiece(game.field, activePiece)
   }
-})
+}
+
+document.addEventListener('keydown', immediateRender)
 
 function play() {
   if (game.isGameOver) {
